72cdcd5bf0c67b68c1844c277c9fdff0192a7f62
Thomas O'Dea <thomaseodea@gmail.com>
2026-01-18 00:42:21 +0000

    remove duplicate typedefs

--- verification/lib/mvu/mvu_pkg.sv
+++ verification/lib/mvu/mvu_pkg.sv
@@ -149,9 +149,4 @@ typedef w_data_t w_data_q_t[$];
 typedef logic [BDBANKW-1 : 0 ] a_data_t;
 typedef a_data_t a_data_q_t[$];
 
-typedef logic [BWBANKW-1 : 0 ] w_data_t;
-typedef w_data_t w_data_q_t[$];
-
-typedef logic [BDBANKW-1 : 0 ] a_data_t;
-typedef a_data_t a_data_q_t[$];
 endpackage
